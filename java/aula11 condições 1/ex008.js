e9c96aa1b5f31ae0eb3feee5bace4ca89e4a1b98
Gabriel Faustino <faustino.gabriel@proton.me>
2023-06-06 17:18:28 -0300

    aula 11

--- java/aula11 condições 1/ex008.js
+++ java/aula11 condições 1/ex008.js
@@ -1,1 +1,8 @@
-console.log('Eu quero beber')
+var vel = 1000
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+// Condição simples
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida! Multado 😡🖕!`)
+} 
+//
+console.log('Dirija com cuidado!')
---
+++ java/aula11 condições 1/ex009.js
@@ -0,0 +1,7 @@
+var country = 'Brasil'
+if (country == 'Brasil') {
+    console.log('Olá, meu compatriota!')
+} else {
+    console.log('Olá, gringo pilantra.')
+}
+
