aff10d284d33aa48da67eab94a1739d7559d4843
Gabriel Faustino <faustino.gabriel@proton.me>
2023-06-12 22:03:13 -0300

    estruturas de repetição

--- java/aula13 repetições/ambiente.js
+++ java/aula13 repetições/ambiente.js
@@ -1,6 +1,12 @@
-console.log('Tudo bem ?')
-console.log('Tudo bem ?')
-console.log('Tudo bem ?')
-console.log('Tudo bem ?')
-console.log('Tudo bem ?')
-console.log('Tudo bem ?')
+
+var c = 1
+while (c <= 500){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
--- java/aula13 repetições/node_92801176e71aa.tmp.js
+++
@@ -1,6 +0,0 @@
-console.log('Tudo bem ?')
-console.log('Tudo bem ?')
-console.log('Tudo bem ?')
-console.log('Tudo bem ?')
-console.log('Tudo bem ?')
-console.log('Tudo bem ?')
