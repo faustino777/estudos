c4ac6b425ca91657677a0dafa2513c11984f29e3
Gabriel Faustino <faustino.gabriel@proton.me>
2023-06-14 00:21:41 -0300

    teste ex

--- java/aula14 exercícios/ex016/script.js
+++ java/aula14 exercícios/ex016/script.js
@@ -1,1 +1,14 @@
-window.alert('Olá')
+var startn = document.getElementById('start').value
+var endn = document.getElementById('end').value
+var passn = document.getElementById('pass').value
+var countb = document.getElementById('count')
+var restxt = document.getElementById('textres')
+countb.addEventListener('click', countn)
+
+function countn() {
+    while (Number(startn) <= Number(endn)){
+        restxt.innerHTML = startn
+        startn += passn
+
+    }
+}
