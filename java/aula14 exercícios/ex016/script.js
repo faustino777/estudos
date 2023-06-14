b5ebc44ef9f767a608ab505d48332361be55e976
Gabriel Faustino <faustino.gabriel@proton.me>
2023-06-14 20:43:22 -0300

    ex16

--- java/aula14 exercícios/ex016/script.js
+++ java/aula14 exercícios/ex016/script.js
@@ -1,14 +1,34 @@
-var startn = document.getElementById('start').value
-var endn = document.getElementById('end').value
-var passn = document.getElementById('pass').value
-var countb = document.getElementById('count')
-var restxt = document.getElementById('textres')
-countb.addEventListener('click', countn)
 
-function countn() {
-    while (Number(startn) <= Number(endn)){
-        restxt.innerHTML = startn
-        startn += passn
+function countnum() {
+    var start = document.getElementById('txtstart')
+    var end = document.getElementById('txtend')
+    var pass = document.getElementById('txtpass')
+    var res = document.getElementById('txtres')
+    
+    if (start.value.length == 0 || end.value.length == 0 || pass.value.length == 0) {
+        window.alert('[ERRO] Faltam dados')
+    } else {
+        res.innerHTML = 'Contando:'
+        var stn = Number(start.value)
+        var edn = Number(end.value)
+        var psn = Number(pass.value)
 
+        if ( psn <= 0) {
+            window.alert('Passo inválido. Considerando passo como 1.')
+            psn = 1
+        }
+        
+        // Contagem crescente
+        if (stn < edn) {
+            for (var c = stn; c <= edn;c += psn) {
+                res.innerHTML += `${c} ➡ `
+            }
+            // Contagem decrescente
+        } else if (stn > edn) {
+            for (var c = stn; c >= edn;c -= psn) {
+                res.innerHTML += `${c} ➡ `
+            } 
+        } res.innerHTML += `⌧`
+        
     }
 }
